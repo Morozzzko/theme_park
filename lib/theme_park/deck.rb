db781f73a57bde1463ce57a38cf837225b8e2ded
Igor S. Morozov <igor@morozov.is>
2019-09-10 00:51:48 +0300

    Refactor deck

--- lib/theme_park/deck.rb
+++ lib/theme_park/deck.rb
@@ -3,31 +3,21 @@
 require 'theme_park/card'
 
 module ThemePark
-  module Deck
-    Type = Types::Array.of(Card)
+  Deck = Types::Array.of(Card)
 
-    class << self
-      def [](*args)
-        Type[*args]
-      end
-
-      def new(*args)
-        Type.new(*args)
-      end
-
-      def create
-        Type[
-          Suit.values.flat_map do |suit|
-            [
-              Ace[suit: suit],
-              King[suit: suit],
-              Queen[suit: suit],
-              Jack[suit: suit],
-              *(2..10).map { |rank| Number[suit: suit, rank: rank] }
-            ]
-          end
-        ].shuffle
-      end
+  class << Deck
+    def create
+      self[
+        Suit.values.flat_map do |suit|
+          [
+            Ace[suit: suit],
+            King[suit: suit],
+            Queen[suit: suit],
+            Jack[suit: suit],
+            *(2..10).map { |rank| Number[suit: suit, rank: rank] }
+          ]
+        end
+      ].shuffle
     end
   end
 end
