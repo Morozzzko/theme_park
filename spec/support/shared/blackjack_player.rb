723083aae62b08feb49e40ce7c420d5b3a742c3e
Igor S. Morozov <igor@morozov.is>
2019-09-16 01:11:15 +0300

    Add dealer strategy (#11)
    * Add dealer strategy
    
    * Move shared examples
    
    * Add take_cards spec
    
    * Move dir
    
    * Rename

---
+++ spec/support/shared/blackjack_player.rb
@@ -0,0 +1,124 @@
+# frozen_string_literal: true
+
+RSpec.shared_examples 'blackjack player' do
+  describe '#blackjack?' do
+    subject(:blackjack?) { player.blackjack? }
+
+    context 'real blackjack' do
+      context 'ace first' do
+        let(:hand) do
+          ThemePark::Deck[
+            [
+              ThemePark::Ace[suit: 'spades'],
+              ThemePark::Number[rank: 10, suit: 'spades']
+            ]
+          ]
+        end
+
+        specify do
+          expect(blackjack?).to be(true)
+        end
+      end
+
+      context 'ace second' do
+        let(:hand) do
+          ThemePark::Deck[
+            [
+              ThemePark::Number[rank: 10, suit: 'spades'],
+              ThemePark::Ace[suit: 'spades']
+            ]
+          ]
+        end
+
+        specify do
+          expect(blackjack?).to be(true)
+        end
+      end
+
+      context 'empty hand' do
+        specify do
+          expect(blackjack?).to be false
+        end
+      end
+
+      context '21, not blackjack' do
+        let(:hand) do
+          ThemePark::Deck[
+            [
+              ThemePark::Number[rank: 10, suit: 'spades'],
+              ThemePark::Ace[suit: 'spades'],
+              ThemePark::Number[rank: 10, suit: 'spades']
+            ]
+          ]
+        end
+
+        specify do
+          expect(blackjack?).to be(false)
+        end
+      end
+    end
+  end
+
+  describe '#sum' do
+    subject(:sum) { player.sum }
+
+    let(:hand) do
+      ThemePark::Deck[
+        [
+          ThemePark::Jack[suit: 'spades'],
+          ThemePark::Queen[suit: 'spades'],
+          ThemePark::King[suit: 'spades']
+        ]
+      ]
+    end
+
+    it 'delegates to #value' do
+      player.hand.each do |card|
+        allow(card).to receive(:value).and_call_original
+      end
+
+      expect(sum).to be(30)
+
+      expect(hand).to all(have_received(:value))
+    end
+
+    describe 'ace' do
+      context 'blackjack' do
+        let(:hand) do
+          [
+            ThemePark::Ace[suit: 'spades'],
+            ThemePark::King[suit: 'spades']
+          ]
+        end
+
+        specify do
+          expect(sum).to be(21)
+        end
+      end
+    end
+  end
+
+  describe 'take_cards' do
+    subject(:take_cards) { player.take_cards(cards) }
+
+    let(:cards) do
+      [
+        ThemePark::King[suit: 'spades'],
+        ThemePark::Queen[suit: 'spades'],
+        ThemePark::Jack[suit: 'spades']
+      ]
+    end
+
+    specify do
+      expect(take_cards).to be_a(described_class)
+      expect(take_cards).not_to be(player)
+      expect(take_cards.hand).to eql(player.hand + cards)
+    end
+
+    context 'busting' do
+      specify do
+        expect(take_cards.state).to be(:bust)
+      end
+    end
+  end
+end
